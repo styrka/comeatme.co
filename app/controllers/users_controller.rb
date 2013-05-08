a694597a829384373b7f45772dd0a0ceb3f1071d
styrka <evan.p.lind@gmail.com>
2013-05-08 12:10:52 -0700

    init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,66 @@
+class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: [:index, :destroy]
+
+  helper_method :admin?
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to(current_user) unless current_user?(@user) || current_user.admin?
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      redirect_to success_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: 'Please sign in.'
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+end
